4476ac67aac699f5af36349b1dbebf2726b967dd
Chester Leung <chester.leung@berkeley.edu>
2020-05-11 03:56:10 +0000

    Add orchestrator demo

---
+++ demo/python/orch-demo/client/run.sh
@@ -0,0 +1,1 @@
+python3 client.py --ip-addr $1 --symmkey ../../../data/key_zeros.txt --privkey ../../../data/userkeys/private_user_1.pem --cert ../../../data/usercrts/user1.crt
---
+++ demo/python/orch-demo/orchestrator/start_orchestrator.py
@@ -0,0 +1,9 @@
+import securexgboost as xgb
+import os
+
+HOME_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../../../"
+
+
+enclave = xgb.Enclave(HOME_DIR + "build/enclave/xgboost_enclave.signed", log_verbosity=0)
+print("Waiting for client...")
+xgb.serve(enclave, all_users=["user1"], nodes=["34.217.70.12"])
