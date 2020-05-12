877147371c6645b6a6b1c7c06f89bbdf199e4518
Chester Leung <chester.leung@berkeley.edu>
2020-05-12 06:30:17 +0000

    No log but stdout

--- enclave/rabit/src/allreduce_base.cc
+++ enclave/rabit/src/allreduce_base.cc
@@ -659,11 +659,11 @@ AllreduceBase::TryAllreduceTree(void *sendrecvbuf_,
 AllreduceBase::ReturnType
 AllreduceBase::TryBroadcast(void *sendrecvbuf_, size_t total_size, int root) {
   RefLinkVector &links = tree_links;
-  LOG(DEBUG) << "We're in trybroadcast\n";
+  std::cout << "We're in trybroadcast\n";
   if (links.size() == 0 || total_size == 0) return kSuccess;
   utils::Check(root < world_size,
                "Broadcast: root should be smaller than world size");
-  LOG(DEBUG) << "Passed check\n";
+  std::cout << "Passed check\n";
   // number of links
   const int nlink = static_cast<int>(links.size());
   // size of space already read from data
@@ -671,19 +671,19 @@ AllreduceBase::TryBroadcast(void *sendrecvbuf_, size_t total_size, int root) {
   // input link, -2 means unknown yet, -1 means this is root
   int in_link = -2;
 
-  LOG(DEBUG) << this->rank << "about to initialize link stats\n";
+  std::cout << this->rank << "about to initialize link stats\n";
   // initialize the link statistics
   for (int i = 0; i < nlink; ++i) {
     links[i].ResetSize();
   }
   // root have all the data
   if (this->rank == root) {
-    LOG(DEBUG) << "Rank " << this->rank << " is the root\n";
+      std::cout << "Rank " << this->rank << " is the root\n";
     size_in = total_size;
     in_link = -1;
   }
   // while we have not passed the messages out
-  LOG(DEBUG) << this->rank << " About to poll...\n"
+  std::cout << this->rank << " About to poll...\n";
   while (true) {
     bool finished = true;
     // select helper
@@ -703,7 +703,7 @@ AllreduceBase::TryBroadcast(void *sendrecvbuf_, size_t total_size, int root) {
       }
       watcher.WatchException(*links[i].sock);
     }
-    LOG(DEBUG) << this->rank << "loop donezo\n";
+    std::cout << this->rank << "loop donezo\n";
     // finish running
     if (finished) break;
     // select
@@ -715,7 +715,7 @@ AllreduceBase::TryBroadcast(void *sendrecvbuf_, size_t total_size, int root) {
         return ReportError(&links[i], kGetExcept);
       }
     }
-    LOG(DEBUG) << this->rank << " Finish exception handling\n";
+    std::cout << this->rank << " Finish exception handling\n";
     if (in_link == -2) {
       // probe in-link
       for (int i = 0; i < nlink; ++i) {
@@ -740,7 +740,7 @@ AllreduceBase::TryBroadcast(void *sendrecvbuf_, size_t total_size, int root) {
         size_in = links[in_link].size_read;
       }
     }
-    LOG(DEBUG) << this->rank << " send data to all out-link\n";
+    std::cout << this->rank << " send data to all out-link\n";
     // send data to all out-link
     for (int i = 0; i < nlink; ++i) {
       if (i != in_link && links[i].size_write < size_in) {
