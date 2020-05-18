5d1bceaf3d9e59caed1e306b8b7efb52503ffb52
Octavian Sima <octavian.sima98@gmail.com>
2020-05-17 17:25:15 -0700

    early_stopping, eval_metrics

---
+++ tests/python/test_early_stopping.py
@@ -0,0 +1,118 @@
+import testing as tm
+import numpy as np
+import unittest
+import pytest
+
+import securexgboost as xgb
+import os
+from sklearn.datasets import dump_svmlight_file
+
+username = "user1"
+HOME_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../"
+sym_key_file = HOME_DIR + "demo/data/key_zeros.txt"
+pub_key_file = HOME_DIR + "demo/data/userkeys/private_user_1.pem"
+cert_file = HOME_DIR + "demo/data/usercrts/{0}.crt".format(username)
+
+temp_name = HOME_DIR + "demo/data/temp_file.txt"
+temp_enc_name = HOME_DIR + "demo/data/temp_file.txt.enc"
+
+print("Init user parameters")
+xgb.init_user(username, sym_key_file, pub_key_file, cert_file)
+
+print("Creating enclave")
+enclave = xgb.Enclave(HOME_DIR + "build/enclave/xgboost_enclave.signed")
+
+# Remote Attestation
+print("Remote attestation")
+
+# Note: Simulation mode does not support attestation
+# pass in `verify=False` to attest()
+enclave.attest(verify=False)
+
+print("Send private key to enclave")
+enclave.add_key()
+
+rng = np.random.RandomState(1994)
+
+
+class TestEarlyStopping(unittest.TestCase):
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def evalerror(self, preds, dtrain):
+        from sklearn.metrics import mean_squared_error
+
+        labels = dtrain.get_label()
+        return 'rmse', mean_squared_error(labels, preds)
+
+    @staticmethod
+    def assert_metrics_length(cv, expected_length):
+        for key, value in cv.items():
+          assert len(value) == expected_length
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def test_cv_early_stopping(self):
+        from sklearn.datasets import load_digits
+
+        digits = load_digits(2)
+        X = digits['data']
+        y = digits['target']
+        dump_svmlight_file(X, y, temp_name) 
+        xgb.encrypt_file(temp_name, temp_enc_name, sym_key_file)
+        dm = xgb.DMatrix({username: temp_enc_name})
+        params = {'max_depth': 2, 'eta': 1, 'verbosity': 0,
+                  'objective': 'binary:logistic'}
+
+        #TODO(rishabh): implement cv()
+        """
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    early_stopping_rounds=10)
+        self.assert_metrics_length(cv, 10)
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    early_stopping_rounds=5)
+        self.assert_metrics_length(cv, 3)
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    early_stopping_rounds=1)
+        self.assert_metrics_length(cv, 1)
+
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    feval=self.evalerror, early_stopping_rounds=10)
+        self.assert_metrics_length(cv, 10)
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    feval=self.evalerror, early_stopping_rounds=1)
+        self.assert_metrics_length(cv, 5)
+        cv = xgb.cv(params, dm, num_boost_round=10, nfold=10,
+                    feval=self.evalerror, maximize=True,
+                    early_stopping_rounds=1)
+        self.assert_metrics_length(cv, 1)
+        """
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def test_cv_early_stopping_with_multiple_eval_sets_and_metrics(self):
+        from sklearn.datasets import load_breast_cancer
+
+        X, y = load_breast_cancer(return_X_y=True)
+        dump_svmlight_file(X, y, temp_name) 
+        xgb.encrypt_file(temp_name, temp_enc_name, sym_key_file)
+        dm = xgb.DMatrix({username: temp_enc_name})
+        params = {'objective':'binary:logistic'}
+
+        metrics = [['auc'], ['error'], ['logloss'],
+                   ['logloss', 'auc'], ['logloss', 'error'], ['error', 'logloss']]
+
+        num_iteration_history = []
+
+        # If more than one metrics is given, early stopping should use the last metric
+        #TODO(rishabh): implement cv()
+        """
+        for i, m in enumerate(metrics):
+            result = xgb.cv(params, dm, num_boost_round=1000, nfold=5, stratified=True,
+                            metrics=m, early_stopping_rounds=20, seed=42)
+            num_iteration_history.append(len(result))
+            df = result['test-{}-mean'.format(m[-1])]
+            # When early stopping is invoked, the last metric should be as best it can be.
+            if m[-1] == 'auc':
+                assert np.all(df <= df.iloc[-1])
+            else:
+                assert np.all(df >= df.iloc[-1])
+        assert num_iteration_history[:3] == num_iteration_history[3:]
+        """
---
+++ tests/python/test_eval_metrics.py
@@ -0,0 +1,149 @@
+import testing as tm
+import numpy as np
+import unittest
+import pytest
+
+import securexgboost as xgb
+import os
+from sklearn.datasets import dump_svmlight_file
+
+username = "user1"
+HOME_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../"
+sym_key_file = HOME_DIR + "demo/data/key_zeros.txt"
+pub_key_file = HOME_DIR + "demo/data/userkeys/private_user_1.pem"
+cert_file = HOME_DIR + "demo/data/usercrts/{0}.crt".format(username)
+
+temp_name_t = HOME_DIR + "demo/data/temp_file_t.txt.train"
+temp_enc_name_t = HOME_DIR + "demo/data/temp_file_t.txt.train.enc"
+temp_name_v = HOME_DIR + "demo/data/temp_file_v.txt.train"
+temp_enc_name_v = HOME_DIR + "demo/data/temp_file_v.txt.train.enc"
+
+print("Init user parameters")
+xgb.init_user(username, sym_key_file, pub_key_file, cert_file)
+
+print("Creating enclave")
+enclave = xgb.Enclave(HOME_DIR + "build/enclave/xgboost_enclave.signed")
+
+# Remote Attestation
+print("Remote attestation")
+
+# Note: Simulation mode does not support attestation
+# pass in `verify=False` to attest()
+enclave.attest(verify=False)
+
+print("Send private key to enclave")
+enclave.add_key()
+
+
+dpath = HOME_DIR + 'demo/data/'
+rng = np.random.RandomState(1337)
+
+
+class TestEvalMetrics(unittest.TestCase):
+     xgb_params_01 = {
+        'verbosity': 0,
+        'nthread': 1,
+        'eval_metric': 'error'
+    }
+
+    xgb_params_02 = {
+        'verbosity': 0,
+        'nthread': 1,
+        'eval_metric': ['error']
+    }
+
+    xgb_params_03 = {
+        'verbosity': 0,
+        'nthread': 1,
+        'eval_metric': ['rmse', 'error']
+    }
+
+    xgb_params_04 = {
+        'verbosity': 0,
+        'nthread': 1,
+        'eval_metric': ['error', 'rmse']
+    }
+
+    def evalerror_01(self, preds, dtrain):
+        labels = dtrain.get_label()
+        return 'error', float(sum(labels != (preds > 0.0))) / len(labels)
+
+    def evalerror_02(self, preds, dtrain):
+        labels = dtrain.get_label()
+        return [('error', float(sum(labels != (preds > 0.0))) / len(labels))]
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def evalerror_03(self, preds, dtrain):
+        from sklearn.metrics import mean_squared_error
+
+        labels = dtrain.get_label()
+        return [('rmse', mean_squared_error(labels, preds)),
+                ('error', float(sum(labels != (preds > 0.0))) / len(labels))]
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def evalerror_04(self, preds, dtrain):
+        from sklearn.metrics import mean_squared_error
+
+        labels = dtrain.get_label()
+        return [('error', float(sum(labels != (preds > 0.0))) / len(labels)),
+                ('rmse', mean_squared_error(labels, preds))]
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def test_eval_metrics(self):
+        try:
+            from sklearn.model_selection import train_test_split
+        except ImportError:
+            from sklearn.cross_validation import train_test_split
+        from sklearn.datasets import load_digits
+
+        digits = load_digits(2)
+        X = digits['data']
+        y = digits['target']
+
+        Xt, Xv, yt, yv = train_test_split(X, y, test_size=0.2, random_state=0)
+
+        dump_svmlight_file(Xt, yt, temp_name_t) 
+        xgb.encrypt_file(temp_name_t, temp_enc_name_t, sym_key_file)
+        dump_svmlight_file(Xv, yv, temp_name_v) 
+        xgb.encrypt_file(temp_name_v, temp_enc_name_v, sym_key_file)
+ 
+        dtrain = xgb.DMatrix({username: temp_name_t})
+        dvalid = xgb.DMatrix({username: temp_name_v})
+
+        watchlist = [(dtrain, 'train'), (dvalid, 'val')]
+
+        gbdt_01 = xgb.train(self.xgb_params_01, dtrain, num_boost_round=10)
+        gbdt_02 = xgb.train(self.xgb_params_02, dtrain, num_boost_round=10)
+        gbdt_03 = xgb.train(self.xgb_params_03, dtrain, num_boost_round=10)
+        assert gbdt_01.predict(dvalid)[0] == gbdt_02.predict(dvalid)[0]
+        assert gbdt_01.predict(dvalid)[0] == gbdt_03.predict(dvalid)[0]
+
+        #TODO(rishabh): implement early_stopping_rounds
+        """
+        gbdt_01 = xgb.train(self.xgb_params_01, dtrain, 10, watchlist,
+                            early_stopping_rounds=2)
+        gbdt_02 = xgb.train(self.xgb_params_02, dtrain, 10, watchlist,
+                            early_stopping_rounds=2)
+        gbdt_03 = xgb.train(self.xgb_params_03, dtrain, 10, watchlist,
+                            early_stopping_rounds=2)
+        gbdt_04 = xgb.train(self.xgb_params_04, dtrain, 10, watchlist,
+                            early_stopping_rounds=2)
+        assert gbdt_01.predict(dvalid)[0] == gbdt_02.predict(dvalid)[0]
+        assert gbdt_01.predict(dvalid)[0] == gbdt_03.predict(dvalid)[0]
+        assert gbdt_03.predict(dvalid)[0] != gbdt_04.predict(dvalid)[0]
+        """
+
+        #TODO(rishabh): implement early_stopping_rounds and feval
+        """
+        gbdt_01 = xgb.train(self.xgb_params_01, dtrain, 10, watchlist,
+                            early_stopping_rounds=2, feval=self.evalerror_01)
+        gbdt_02 = xgb.train(self.xgb_params_02, dtrain, 10, watchlist,
+                            early_stopping_rounds=2, feval=self.evalerror_02)
+        gbdt_03 = xgb.train(self.xgb_params_03, dtrain, 10, watchlist,
+                            early_stopping_rounds=2, feval=self.evalerror_03)
+        gbdt_04 = xgb.train(self.xgb_params_04, dtrain, 10, watchlist,
+                            early_stopping_rounds=2, feval=self.evalerror_04)
+        assert gbdt_01.predict(dvalid)[0] == gbdt_02.predict(dvalid)[0]
+        assert gbdt_01.predict(dvalid)[0] == gbdt_03.predict(dvalid)[0]
+        assert gbdt_03.predict(dvalid)[0] != gbdt_04.predict(dvalid)[0]
+        """
