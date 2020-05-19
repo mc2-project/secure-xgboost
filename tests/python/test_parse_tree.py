fd4eaa289377e5f8a7a6de19fb662aa269f00e26
Octavian Sima <octavian.sima98@gmail.com>
2020-05-18 20:31:09 -0700

    parse_tree

---
+++ tests/python/test_parse_tree.py
@@ -0,0 +1,81 @@
+import unittest
+import numpy as np
+import pytest
+import testing as tm
+
+
+pytestmark = pytest.mark.skipif(**tm.no_pandas())
+
+
+import securexgboost as xgb
+import os
+from sklearn.datasets import dump_svmlight_file
+
+username = "user1"
+HOME_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../"
+sym_key_file = HOME_DIR + "demo/data/key_zeros.txt"
+pub_key_file = HOME_DIR + "demo/data/userkeys/private_user_1.pem"
+cert_file = HOME_DIR + "demo/data/usercrts/{0}.crt".format(username)
+
+temp_name = HOME_DIR + "demo/data/temp_file.txt"
+temp_enc_name = HOME_DIR + "demo/data/temp_file.txt.enc"
+
+print("Init user parameters")
+xgb.init_user(username, sym_key_file, pub_key_file, cert_file)
+
+print("Creating enclave")
+enclave = xgb.Enclave(HOME_DIR + "build/enclave/xgboost_enclave.signed")
+
+# Remote Attestation
+print("Remote attestation")
+
+# Note: Simulation mode does not support attestation
+# pass in `verify=False` to attest()
+enclave.attest(verify=False)
+
+print("Send private key to enclave")
+enclave.add_key()
+
+
+dpath = HOME_DIR + 'demo/data/'
+rng = np.random.RandomState(1994)
+
+
+class TestTreesToDataFrame(unittest.TestCase):
+
+    def build_model(self, max_depth, num_round):
+        dtrain = xgb.DMatrix({username: dpath + 'agaricus.txt.train'})
+        param = {'max_depth': max_depth, 'objective': 'binary:logistic',
+                 'verbosity': 1}
+        num_round = num_round
+        bst = xgb.train(param, dtrain, num_round)
+        return bst
+
+    def parse_dumped_model(self, booster, item_to_get, splitter):
+        item_to_get += '='
+        txt_dump = booster.get_dump(with_stats=True)
+        tree_list = [tree.split('/n') for tree in txt_dump]
+        split_trees = [tree[0].split(item_to_get)[1:] for tree in tree_list]
+        res = sum([float(line.split(splitter)[0])
+                   for tree in split_trees for line in tree])
+        return res
+
+    def test_trees_to_dataframe(self):
+        bst = self.build_model(max_depth=5, num_round=10)
+        gain_from_dump = self.parse_dumped_model(booster=bst,
+                                                 item_to_get='gain',
+                                                 splitter=',')
+        cover_from_dump = self.parse_dumped_model(booster=bst,
+                                                  item_to_get='cover',
+                                                  splitter='\n')
+        # method being tested
+        df = bst.trees_to_dataframe()
+
+        # test for equality of gains
+        gain_from_df = df[df.Feature != 'Leaf'][['Gain']].sum()
+        assert np.allclose(gain_from_dump, gain_from_df)
+
+        # test for equality of covers
+        cover_from_df = df.Cover.sum()
+        assert np.allclose(cover_from_dump, cover_from_df)
+
