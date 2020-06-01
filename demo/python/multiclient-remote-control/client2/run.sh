1755204304c939a49bccdc33f97229077a027a1b
Rishabh Poddar <rishabhp89@gmail.com>
2020-06-01 14:43:10 -0700

    Improve management of credentials (#76)
    * Error handling for API
    
    * Helper scripts to auto-generate credentials
    
    * Typos
    
    * Update keys in demos
    
    * Load model in demo

--- demo/python/multiclient-remote-control/client2/run.sh
+++ demo/python/multiclient-remote-control/client2/run.sh
@@ -1,1 +1,1 @@
-python3 client2.py --ip-addr $1 --symmkey key2.txt --privkey ../../../data/userkeys/private_user_2.pem --cert ../../../data/usercrts/user2.crt
+python3 client2.py --ip-addr $1 --symmkey key2.txt --privkey ../../../../config/user2.pem --cert ../../../../config/user2.crt
--- scripts/gen-self-signed-ca.sh
+++
@@ -1,4 +0,0 @@
-mbedtls/programs/x509/cert_write selfsign=1 issuer_key=keypair.pem                    \
-                         issuer_name=CN=securexgboost,O=riselab,C=NL        \
-                         not_before=20130101000000 not_after=20251231235959   \
-                         is_ca=1 max_pathlen=0 output_file=CA.crt
