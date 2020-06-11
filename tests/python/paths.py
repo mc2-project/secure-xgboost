af56d3a6136ee011fb198f8eb0968ec7450ac8e9
Chester Leung <chester.leung@berkeley.edu>
2020-06-11 23:41:17 +0000

    Modify paths to keys and certificate, securexgboost API

---
+++ tests/python/paths.py
@@ -0,0 +1,6 @@
+import os
+
+HOME_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../"
+sym_key_file = HOME_DIR + "demo/data/key_zeros.txt"
+priv_key_file = HOME_DIR + "demo/data/userkeys/private_user_1.pem"
+cert_file = HOME_DIR + "demo/data/usercrts/user1.crt"
