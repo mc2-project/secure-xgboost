01652077ffb5dbd081765d3ae819b57bf8132df3
Rishabh Poddar <rishabhp89@gmail.com>
2020-08-31 23:13:58 -0700

    Add 10 year sample certs (#111)

--- config/gen-signer.sh
+++ config/gen-signer.sh
@@ -6,4 +6,4 @@ openssl genrsa -out signer.pem -3 3072
 openssl rsa -in signer.pem -pubout -out signer.pub
 
 # Generate self-signed root certificate
-openssl req -x509 -new -nodes -key signer.pem -sha256 -days 365 -out signer.crt -subj "/CN=root"
+openssl req -x509 -new -nodes -key signer.pem -sha256 -days 3650 -out signer.crt -subj "/CN=root"
