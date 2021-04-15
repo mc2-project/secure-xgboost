71c00b7c597a7145aae179980052bff79ec61846
Chester Leung <chester.leung@berkeley.edu>
2021-04-15 12:17:23 -0700

    Fixes to distributed training (#151)
    * Share public key when sharing private key, nonce, symmetric key on rabit init

--- enclave/include/enclave_context.h
+++ enclave/include/enclave_context.h
@@ -447,6 +447,7 @@ class EnclaveContext {
     void share_keys_and_nonce() {
         rabit::Broadcast(m_symm_key, CIPHER_KEY_SIZE, 0);
         rabit::Broadcast(m_nonce, CIPHER_IV_SIZE, 0);
+        rabit::Broadcast(m_public_key, CIPHER_PK_SIZE, 0);
 
         size_t private_key_length;
         unsigned char m_private_key[5 * CIPHER_PK_SIZE];
